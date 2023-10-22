f352f7b956cb06be0f60c07ab95dcd7047045649
ProFilo_is_Back <filippomari18@gmail.com>
2023-10-22 22:27:20 +0200

    Create python_collector.py

---
+++ python_collector.py
@@ -0,0 +1,17 @@
+# STRING
+flowers = "pink primrose,hard-leaved pocket orchid,canterbury bells,sweet pea,english marigold,tiger lily,moon orchid,bird of paradise,monkshood,globe thistle"
+
+
+# LIST
+flowers_list = ["pink primrose", "hard-leaved pocket orchid", "canterbury bells", "sweet pea", "english marigold", "tiger lily", "moon orchid", "bird of paradise", "monkshood", "globe thistle"]
+print("First entry:", flowers_list[0])
+
+# pulling only some elements of the list
+print("First three entries:", flowers_list[:3])
+print("Final two entries:", flowers_list[-2:])
+
+# remove items
+flowers_list.remove("globe thistle")
+
+#add items
+flowers_list.append("snapdragon")
