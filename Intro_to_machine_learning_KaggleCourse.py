a1cc6872bb6336c1790c2fcd1fd975a42e014e8d
ProFilo_is_Back <filippomari18@gmail.com>
2023-10-31 23:51:15 +0100

    Update Intro_to_machine_learning_KaggleCourse.py

--- Intro_to_machine_learning_KaggleCourse.py
+++ Intro_to_machine_learning_KaggleCourse.py
@@ -7,20 +7,47 @@ melbourne_data = pd.read_csv(melbourne_file_path)
 # print a summary of the data in Melbourne data
 melbourne_data.describe()
 
-#exercise
-# Set up code checking
-from learntools.core import binder
-binder.bind(globals())
-from learntools.machine_learning.ex2 import *
-print("Setup Complete")
-
 import pandas as pd
 
-# Path of the file to read
-iowa_file_path = '../input/home-data-for-ml-course/train.csv'
+melbourne_file_path = '../input/melbourne-housing-snapshot/melb_data.csv'
+melbourne_data = pd.read_csv(melbourne_file_path) 
+melbourne_data.columns
 
-# Fill in the line below to read the file into a variable home_data
-home_data = ____
+# The Melbourne data has some missing values (some houses for which some variables weren't recorded.)
+# We'll learn to handle missing values in a later tutorial.  
+# Your Iowa data doesn't have missing values in the columns you use. 
+# So we will take the simplest option for now, and drop houses from our data. 
+# Don't worry about this much for now, though the code is:
 
-# Call line below with no argument to check that you've loaded the data correctly
-step_1.check()
+# dropna drops missing values (think of na as "not available")
+melbourne_data = melbourne_data.dropna(axis=0)
+
+# selecting multiple features by provising a list of column names inside brackets
+melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
+X = melbourne_data[melbourne_features]
+
+# let's review the data we are going to use for prediction
+X.describe()
+X.head()
+
+'''
+Define: What type of model will it be? A decision tree? Some other type of model? Some other parameters of the model type are specified too.
+Fit: Capture patterns from provided data. This is the heart of modeling.
+Predict: Just what it sounds like
+Evaluate: Determine how accurate the model's predictions are.
+'''
+
+from sklearn.tree import DecisionTreeRegressor
+
+# Define model. Specify a number for random_state to ensure same results each run
+melbourne_model = DecisionTreeRegressor(random_state=1)
+
+# Fit model
+melbourne_model.fit(X, y)
+
+
+print("Making predictions for the following 5 houses:")
+print(X.head())
+print("The predictions are")
+print(melbourne_model.predict(X.head()))
+
