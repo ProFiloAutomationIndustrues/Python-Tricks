72454ef48c894156846506ee7f366a29b2bcc6cd
ProFilo_is_Back <filippomari18@gmail.com>
2023-11-03 00:24:09 +0100

    Update Intro_to_machine_learning_KaggleCourse.py

--- Intro_to_machine_learning_KaggleCourse.py
+++ Intro_to_machine_learning_KaggleCourse.py
@@ -123,4 +123,31 @@ print(mean_absolute_error(val_y, val_predictions))
 
 
 
+ESERCIZIO DI MODEL VALIDATION
+# Import the train_test_split function and uncomment
+from sklearn.model_selection import train_test_split
+
+# fill in and uncomment
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state = 1) #Ensure you set the random_state to 1
+# You imported DecisionTreeRegressor in your last exercise
+# and that code has been copied to the setup code above. So, no need to
+# import it again
+
+# Specify the model
+iowa_model = DecisionTreeRegressor(random_state = 1)
+
+# Fit iowa_model with the training data.
+iowa_model.fit(train_X, train_y)
+# Predict with all validation observations
+val_predictions = iowa_model.predict(val_X)
+from sklearn.metrics import mean_absolute_error
+val_mae = mean_absolute_error(val_y, val_predictions)
+
+# uncomment following line to see the validation_mae
+print(val_mae)
+
+
+
+
+
 
