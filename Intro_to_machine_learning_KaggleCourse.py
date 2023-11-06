d46a296a5a92530147da442d81b4abb1850dad95
ProFilo_is_Back <filippomari18@gmail.com>
2023-11-06 23:18:00 +0100

    Update Intro_to_machine_learning_KaggleCourse.py

--- Intro_to_machine_learning_KaggleCourse.py
+++ Intro_to_machine_learning_KaggleCourse.py
@@ -279,7 +279,7 @@ print("Min mae: {:.2f}".format(min(mae_list)))
 best_tree_size = candidate_max_leaf_nodes[mae_list.index(min(mae_list))]
 print("Best tree size: %d" %(best_tree_size))
 
-# ONce we have found the optimal tree depth, let's train a new decision tree regressor model with all
+# Once we have found the optimal tree depth, let's train a new decision tree regressor model with all
 # the train data, to make it even more accurate.
 # optimal size
 final_model = DecisionTreeRegressor(max_leaf_nodes = best_tree_size, random_state = 0)
@@ -288,6 +288,80 @@ final_model.fit(X, y)
 
 # Decision tree models are not very sophisticated by modern machine learning standars. 
 # Let's use random forests to improve  our models even more.
+RandomForestRegressor()
+forest_model = RandomForestRegressor(random_state=1)
+forest_model.fit(train_X, train_y)
+melb_preds = forest_model.predict(val_X)
+print(mean_absolute_error(val_y, melb_preds))
+
+# one of the best features of Random Forest models is that they generally work reasonably 
+# with default parameters.
+
+# EXERCISE RANDOM FORESTS
+# Prima trainiamo comunque il DecisionTreeRegressor con optimal_size, per fare poi il confronto con il 
+# modello successivo.
+
+# Code you have previously used to load data
+import pandas as pd
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+​
+​
+# Path of the file to read
+iowa_file_path = '../input/home-data-for-ml-course/train.csv'
+​
+home_data = pd.read_csv(iowa_file_path)
+# Create target object and call it y
+y = home_data.SalePrice
+# Create X
+features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
+X = home_data[features]
+​
+# Split into validation and training data
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+​
+# Specify Model
+iowa_model = DecisionTreeRegressor(random_state=1) # NOT SPECIFYING max_leaf_nodes
+# Fit Model
+iowa_model.fit(train_X, train_y)
+​
+# Make validation predictions and calculate mean absolute error
+val_predictions = iowa_model.predict(val_X)
+val_mae = mean_absolute_error(val_predictions, val_y)
+print("Validation MAE when not specifying max_leaf_nodes: {:.2f}".format(val_mae))
+​
+# Using best value for max_leaf_nodes
+iowa_model = DecisionTreeRegressor(max_leaf_nodes = 100, random_state = 1) # SPECIFYING max_leaf_nodes
+iowa_model.fit(train_X, train_y)
+val_predictions = iowa_model.predict(val_X)
+val_mae = mean_absolute_error(val_predictions, val_y)
+print("Validation MAE for best value of max_leaf_nodes: {:.2f}".format(val_mae))
+​
+​
+# Set up code checking
+from learntools.core import binder
+binder.bind(globals())
+from learntools.machine_learning.ex6 import *
+print("\nSetup complete")
+
+from sklearn.ensemble import RandomForestRegressor
+
+# Define the model. Set random_state to 1
+rf_model = RandomForestRegressor(random_state = 1)
+
+# fit your model
+rf_model.fit(train_X, train_y)
+
+# Calculate the mean absolute error of your Random Forest model on the validation data
+rf_val_predictions = rf_model.predict(val_X)
+rf_val_mae = mean_absolute_error(val_y, rf_val_predictions)
+
+print("Validation MAE for Random Forest Model: {:.2f}".format(rf_val_mae))
+
+
+
+
 
 
 
