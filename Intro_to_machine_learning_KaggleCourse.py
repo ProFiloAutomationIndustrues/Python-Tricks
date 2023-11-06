becfc2ab64dd5213c617888943d9b763df04aecc
ProFilo_is_Back <filippomari18@gmail.com>
2023-11-06 22:48:51 +0100

    Update Intro_to_machine_learning_KaggleCourse.py

--- Intro_to_machine_learning_KaggleCourse.py
+++ Intro_to_machine_learning_KaggleCourse.py
@@ -122,6 +122,32 @@ val_predictions = melbourne_model.predict(val_X)
 print(mean_absolute_error(val_y, val_predictions))
 
 
+# Code you have previously used to load data
+import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
+
+# Path of the file to read
+iowa_file_path = '../input/home-data-for-ml-course/train.csv'
+
+home_data = pd.read_csv(iowa_file_path)
+y = home_data.SalePrice
+feature_columns = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
+X = home_data[feature_columns]
+
+# Specify Model
+iowa_model = DecisionTreeRegressor()
+# Fit Model
+iowa_model.fit(X, y)
+
+print("First in-sample predictions:", iowa_model.predict(X.head()))
+print("Actual target values for those homes:", y.head().tolist())
+
+# Set up code checking
+from learntools.core import binder
+binder.bind(globals())
+from learntools.machine_learning.ex4 import *
+print("Setup Complete")
+
 
 ESERCIZIO DI MODEL VALIDATION
 # Import the train_test_split function and uncomment
@@ -146,6 +172,128 @@ val_mae = mean_absolute_error(val_y, val_predictions)
 # uncomment following line to see the validation_mae
 print(val_mae)
 
+# UNDERFITTING AND OVERFITTING #
+Overfitting: capturing spurious patterns that won't recur in the future, leading to less accurate predictions, or
+Underfitting: failing to capture relevant patterns, again leading to less accurate predictions.
+
+# utility function to compare MAE (mean absolute error) scores from different values of max_leaf_nodes parameter.
+from sklearn.metrics import mean_absolute_error
+from sklearn.tree import DecisionTreeRegressor
+
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    # definisco un modello con un certo livello di profondità degli alberi
+    model.fit(train_X, train_y)
+    # alleno il modello con set di train
+    preds_val = model.predict(val_X)
+    # calcolo predictions con restante set di dati
+    mae = mean_absolute_error(val_y, preds_val)
+    # calcolo del mae
+    return(mae)
+
+# Let's use a for loop to compare some results depending on max_leaf_nodes parameter.
+# Data Loading Code Runs At This Point
+import pandas as pd
+    
+# Load data
+melbourne_file_path = '../input/melbourne-housing-snapshot/melb_data.csv'
+melbourne_data = pd.read_csv(melbourne_file_path) 
+# Filter rows with missing values
+filtered_melbourne_data = melbourne_data.dropna(axis=0)
+# Choose target and features
+y = filtered_melbourne_data.Price # seleziono la colonna del Price, che è obiettivo da predirre
+melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'BuildingArea', 
+                        'YearBuilt', 'Lattitude', 'Longtitude']
+X = filtered_melbourne_data[melbourne_features] # qui prendo le features che mi interessano per trainare il modello
+
+from sklearn.model_selection import train_test_split
+# split data into training and validation data, for both features and target
+train_X, val_X, train_y, val_y = train_test_split(X, y,random_state = 0)
+
+# for loop to compare different mae depending on max_leaf_nodes parameters
+# compare MAE with differing values of max_leaf_nodes
+for max_leaf_nodes in [5, 50, 500, 5000]:
+    my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
+    print("Max leaf nodes: %d  \t\t Mean Absolute Error:  %d" %(max_leaf_nodes, my_mae))
+
+
+# EXERCISE
+# Code you have previously used to load data
+import pandas as pd
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+
+
+# Path of the file to read
+iowa_file_path = '../input/home-data-for-ml-course/train.csv'
+
+home_data = pd.read_csv(iowa_file_path)
+# Create target object and call it y
+y = home_data.SalePrice
+# Create X
+features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
+X = home_data[features]
+
+# Split into validation and training data
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+
+# Specify Model
+iowa_model = DecisionTreeRegressor(random_state=1)
+# Fit Model
+iowa_model.fit(train_X, train_y)
+
+# Make validation predictions and calculate mean absolute error
+val_predictions = iowa_model.predict(val_X)
+val_mae = mean_absolute_error(val_predictions, val_y)
+print("Validation MAE: {:,.0f}".format(val_mae))
+
+# Set up code checking
+from learntools.core import binder
+binder.bind(globals())
+from learntools.machine_learning.ex5 import *
+print("\nSetup complete")
+
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+  model = DecisionTreeRegressor(max_leaf_nodes = max_leaf_nodes, random_state = 0)
+  model.fit(train_X, train_y)
+  preds_val = model.predict(val_X)
+  mae = mean_absolute_error(val_y, preds_val) # comparing reference labels wrt predicted labels
+  return(mae)
+
+# kaggle solution
+scores = {leaf_size: get_mae(leaf_size, train_X, val_X, train_y, val_y) for leaf_size in candidate_max_leaf_nodes}
+best_tree_size = min(scores, key=scores.get)
+
+# mine solution
+candidate_max_leaf_nodes = [5, 25, 50, 100, 250, 500]
+# Write loop to find the ideal tree size from candidate_max_leaf_nodes
+mae_list = []
+for max_leaf_nodes in candidate_max_leaf_nodes:
+    mae_list.append(get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y))
+    
+print(mae_list)
+print("Min mae: {:.2f}".format(min(mae_list)))
+
+# Store the best value of max_leaf_nodes (it will be either 5, 25, 50, 100, 250 or 500)
+best_tree_size = candidate_max_leaf_nodes[mae_list.index(min(mae_list))]
+print("Best tree size: %d" %(best_tree_size))
+
+# ONce we have found the optimal tree depth, let's train a new decision tree regressor model with all
+# the train data, to make it even more accurate.
+# optimal size
+final_model = DecisionTreeRegressor(max_leaf_nodes = best_tree_size, random_state = 0)
+
+final_model.fit(X, y)
+
+# Decision tree models are not very sophisticated by modern machine learning standars. 
+# Let's use random forests to improve  our models even more.
+
+
+
+
+
+  
 
 
 
