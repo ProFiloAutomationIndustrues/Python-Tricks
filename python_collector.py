2c038ca1c318db9f804bc38366837e1678ec0e64
ProFilo_is_Back <filippomari18@gmail.com>
2023-11-06 22:30:09 +0100

    Update python_collector.py

--- python_collector.py
+++ python_collector.py
@@ -15,7 +15,7 @@ flowers_list.remove("globe thistle")
 
 #add items
 flowers_list.append("snapdragon")
-
+mylist1.index(min(mylist1)) #get minimum of a list and its position in the list
 
 print("Validation MAE: {:,.0f}".format(val_mae))
 print("Max leaf nodes: %d  \t\t Mean Absolute Error:  %d" %(max_leaf_nodes, my_mae))
