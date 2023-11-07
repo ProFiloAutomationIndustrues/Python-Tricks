fc2ceb2f30c584ad9b969c7e11b1ffcc76593f2b
ProFilo_is_Back <filippomari18@gmail.com>
2023-11-07 23:04:58 +0100

    Update Intermediate_machine_learning_course.py

--- Intermediate_machine_learning_course.py
+++ Intermediate_machine_learning_course.py
@@ -113,3 +113,74 @@ imputed_X_valid_plus.columns = X_valid_plus.columns
 
 print("MAE from Approach 3 (An Extension to Imputation):")
 print(score_dataset(imputed_X_train_plus, imputed_X_valid_plus, y_train, y_valid))
+
+# EXERCISE MISSING VALUES
+# Set up code checking
+import os
+if not os.path.exists("../input/train.csv"):
+    os.symlink("../input/home-data-for-ml-course/train.csv", "../input/train.csv")  
+    os.symlink("../input/home-data-for-ml-course/test.csv", "../input/test.csv") 
+from learntools.core import binder
+binder.bind(globals())
+from learntools.ml_intermediate.ex2 import *
+print("Setup Complete")
+
+# loading data from predict house prices comptetition
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# Read the data
+X_full = pd.read_csv('../input/train.csv', index_col='Id')
+X_test_full = pd.read_csv('../input/test.csv', index_col='Id')
+
+# Remove rows with missing target, separate target from predictors
+X_full.dropna(axis=0, subset=['SalePrice'], inplace=True)
+y = X_full.SalePrice
+X_full.drop(['SalePrice'], axis=1, inplace=True)
+
+# To keep things simple, we'll use only numerical predictors
+X = X_full.select_dtypes(exclude=['object'])
+X_test = X_test_full.select_dtypes(exclude=['object'])
+
+# Break off validation set from training data
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2, random_state=0)
+X_train.head()
+
+# Shape of training data (num_rows, num_columns)
+print(X_train.shape)
+
+# Number of missing values in each column of training data
+missing_val_count_by_column = (X_train.isnull().sum())
+print(missing_val_count_by_column[missing_val_count_by_column > 0])
+
+# Fill in the line below: How many rows are in the training data?
+num_rows = X_train.shape[0] 
+print("Num rows: %d" %(num_rows))
+
+# Fill in the line below: How many columns in the training data
+# have missing values?
+num_cols_with_missing = missing_val_count_by_column[missing_val_count_by_column > 0] #I've created a list
+print("Num cols with missing: %d" %(len(num_cols_with_missing)))
+
+# Fill in the line below: How many missing entries are contained in 
+# all of the training data?
+tot_missing = int(num_cols_with_missing.sum())
+print("Total missing elements: %d" %(tot_missing))
+
+#Since there are relatively few missing entries in the data (the column with the greatest percentage of missing values is missing less than 20% of its entries), 
+#we can expect that dropping columns is unlikely to yield good results. This is because we'd be throwing away a lot of valuable data, and so imputation will likely perform better.
+
+# bisogna valutare se una feature ha veramente poche entries o no. CIoè se, non considerando quella colonna, effettivamente si vanno a perdere molti dati. Oppure se magari, come sopra,
+# la percentuale dei dati mancanti è poca, allora conviene fare imputation per non buttare via il resto dei dati.
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+
+# Function for comparing different approaches
+def score_dataset(X_train, X_valid, y_train, y_valid):
+    model = RandomForestRegressor(n_estimators=100, random_state=0)
+    model.fit(X_train, y_train)
+    preds = model.predict(X_valid)
+    return mean_absolute_error(y_valid, preds)
+
+
+
