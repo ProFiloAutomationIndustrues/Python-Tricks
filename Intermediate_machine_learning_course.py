055cbe5636323e7ee4529e765570a88e9fd395a4
ProFilo_is_Back <filippomari18@gmail.com>
2023-12-04 23:52:41 +0100

    Update Intermediate_machine_learning_course.py

--- Intermediate_machine_learning_course.py
+++ Intermediate_machine_learning_course.py
@@ -270,5 +270,471 @@ output = pd.DataFrame({'Id': X_test.index,
                        'SalePrice': preds_test})
 output.to_csv('submission.csv', index=False)
 
+## CATEGORICAL VARIABLES ##
+# Theory
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# Read the data
+data = pd.read_csv('../input/melbourne-housing-snapshot/melb_data.csv')
+
+# Separate target from predictors
+y = data.Price
+X = data.drop(['Price'], axis=1)
+
+# Divide data into training and validation subsets
+X_train_full, X_valid_full, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2,
+                                                                random_state=0)
+
+# Drop columns with missing values (simplest approach)
+cols_with_missing = [col for col in X_train_full.columns if X_train_full[col].isnull().any()] 
+X_train_full.drop(cols_with_missing, axis=1, inplace=True)
+X_valid_full.drop(cols_with_missing, axis=1, inplace=True)
+
+# "Cardinality" means the number of unique values in a column
+# Select categorical columns with relatively low cardinality (convenient but arbitrary)
+low_cardinality_cols = [cname for cname in X_train_full.columns if X_train_full[cname].nunique() < 10 and 
+                        X_train_full[cname].dtype == "object"]
+
+# Select numerical columns
+numerical_cols = [cname for cname in X_train_full.columns if X_train_full[cname].dtype in ['int64', 'float64']]
+
+# Keep selected columns only
+my_cols = low_cardinality_cols + numerical_cols
+X_train = X_train_full[my_cols].copy()
+X_valid = X_valid_full[my_cols].copy()
+
+# Get list of categorical variables
+s = (X_train.dtypes == 'object')
+object_cols = list(s[s].index)
+
+print("Categorical variables:")
+print(object_cols)
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+
+# Function for comparing different approaches
+def score_dataset(X_train, X_valid, y_train, y_valid):
+    model = RandomForestRegressor(n_estimators=100, random_state=0)
+    model.fit(X_train, y_train)
+    preds = model.predict(X_valid)
+    return mean_absolute_error(y_valid, preds)
+
+# score from approach 1 (drop categorical variables)
+drop_X_train = X_train.select_dtypes(exclude=['object'])
+drop_X_valid = X_valid.select_dtypes(exclude=['object'])
+
+print("MAE from Approach 1 (Drop categorical variables):")
+print(score_dataset(drop_X_train, drop_X_valid, y_train, y_valid))
+
+# score from approach 2 (ordinal encoding)
+from sklearn.preprocessing import OrdinalEncoder
+
+# Make copy to avoid changing original data 
+label_X_train = X_train.copy()
+label_X_valid = X_valid.copy()
+
+# Apply ordinal encoder to each column with categorical data
+ordinal_encoder = OrdinalEncoder()
+label_X_train[object_cols] = ordinal_encoder.fit_transform(X_train[object_cols])
+label_X_valid[object_cols] = ordinal_encoder.transform(X_valid[object_cols])
+
+print("MAE from Approach 2 (Ordinal Encoding):") 
+print(score_dataset(label_X_train, label_X_valid, y_train, y_valid))
+
+# score from approach 3 (One HOT Encoding)
+from sklearn.preprocessing import OneHotEncoder
+
+# Apply one-hot encoder to each column with categorical data
+OH_encoder = OneHotEncoder(handle_unknown='ignore', sparse=False)
+OH_cols_train = pd.DataFrame(OH_encoder.fit_transform(X_train[object_cols]))
+OH_cols_valid = pd.DataFrame(OH_encoder.transform(X_valid[object_cols]))
+
+# One-hot encoding removed index; put it back
+OH_cols_train.index = X_train.index
+OH_cols_valid.index = X_valid.index
+
+# Remove categorical columns (will replace with one-hot encoding)
+num_X_train = X_train.drop(object_cols, axis=1)
+num_X_valid = X_valid.drop(object_cols, axis=1)
+
+# Add one-hot encoded columns to numerical features
+OH_X_train = pd.concat([num_X_train, OH_cols_train], axis=1)
+OH_X_valid = pd.concat([num_X_valid, OH_cols_valid], axis=1)
+
+# Ensure all columns have string type
+OH_X_train.columns = OH_X_train.columns.astype(str)
+OH_X_valid.columns = OH_X_valid.columns.astype(str)
+
+print("MAE from Approach 3 (One-Hot Encoding):") 
+print(score_dataset(OH_X_train, OH_X_valid, y_train, y_valid))
+
+
+
+# Practice
+# Set up code checking
+import os
+if not os.path.exists("../input/train.csv"):
+    os.symlink("../input/home-data-for-ml-course/train.csv", "../input/train.csv")  
+    os.symlink("../input/home-data-for-ml-course/test.csv", "../input/test.csv") 
+from learntools.core import binder
+binder.bind(globals())
+from learntools.ml_intermediate.ex3 import *
+print("Setup Complete")
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# Read the data
+X = pd.read_csv('../input/train.csv', index_col='Id') 
+X_test = pd.read_csv('../input/test.csv', index_col='Id')
+
+# Remove rows with missing target, separate target from predictors
+X.dropna(axis=0, subset=['SalePrice'], inplace=True)
+y = X.SalePrice
+X.drop(['SalePrice'], axis=1, inplace=True)
+
+# To keep things simple, we'll drop columns with missing values
+cols_with_missing = [col for col in X.columns if X[col].isnull().any()] 
+X.drop(cols_with_missing, axis=1, inplace=True)
+X_test.drop(cols_with_missing, axis=1, inplace=True)
+
+# Break off validation set from training data
+X_train, X_valid, y_train, y_valid = train_test_split(X, y,
+                                                      train_size=0.8, test_size=0.2,
+                                                      random_state=0)
+X_train.head()
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+
+# function for comparing different approaches
+def score_dataset(X_train, X_valid, y_train, y_valid):
+    model = RandomForestRegressor(n_estimators=100, random_state=0)
+    model.fit(X_train, y_train)
+    preds = model.predict(X_valid)
+    return mean_absolute_error(y_valid, preds)
+
+## Step 1: Drop columns with categorical data
+# Fill in the lines below: drop columns in training and validation data
+drop_X_train = X_train.select_dtypes(exclude = ['object'])
+drop_X_valid = X_valid.select_dtypes(exclude = ['object'])
+print("MAE from Approach 1 (Drop categorical variables):")
+print(score_dataset(drop_X_train, drop_X_valid, y_train, y_valid))
+
+MAE from Approach 1 (Drop categorical variables):
+17837.82570776256
+
+print("Unique values in 'Condition2' column in training data:", X_train['Condition2'].unique())
+print("\nUnique values in 'Condition2' column in validation data:", X_valid['Condition2'].unique())
+Occhio perché potresti fare l'allenamento del modello su categorie che poi magari non hai in validazione...
+Forse questo vuole far notare l'esercizio
+
+Il problema è appunto fare una classificazione sui dati di trainign, e ppoi ti trovi quelli di validazione
+e test che magari non hanno quella categoria specifica.
+Are there any values that appear in the validation data but not in the training data?
+
+## Step 2: Ordinal encoding
+Ci mettiamo al riparo da questi problemi: drop problematic categorical columns.
+  
+# PART A
+# Categorical columns in the training data
+object_cols = [col for col in X_train.columns if X_train[col].dtype == "object"]
+
+# Columns that can be safely ordinal encoded
+good_label_cols = [col for col in object_cols if 
+                   set(X_valid[col]).issubset(set(X_train[col]))]
+        
+# Problematic columns that will be dropped from the dataset
+bad_label_cols = list(set(object_cols)-set(good_label_cols))
+        
+print('Categorical columns that will be ordinal encoded:', good_label_cols)
+print('\nCategorical columns that will be dropped from the dataset:', bad_label_cols)
+
+# PART B
+#You should ordinal encode the categorical columns in good_label_cols.
+  
+from sklearn.preprocessing import OrdinalEncoder
+
+# Drop categorical columns that will not be encoded
+label_X_train = X_train.drop(bad_label_cols, axis=1)
+label_X_valid = X_valid.drop(bad_label_cols, axis=1)
+
+label_X_train_copy = label_X_train.copy()
+label_X_valid_copy = label_X_valid.copy()
+
+# Apply ordinal encoder 
+ordinal_encoder = OrdinalEncoder()
+label_X_train[good_label_cols] = ordinal_encoder.fit_transform(label_X_train[good_label_cols])
+label_X_valid[good_label_cols] = ordinal_encoder.fit_transform(label_X_valid[good_label_cols])
+
+print("MAE from Approach 2 (Ordinal Encoding):") 
+print(score_dataset(label_X_train, label_X_valid, y_train, y_valid))
+
+MAE from Approach 2 (Ordinal Encoding):
+17274.866860730595
+
+# Get number of unique entries in each column with categorical data
+object_nunique = list(map(lambda col: X_train[col].nunique(), object_cols))
+d = dict(zip(object_cols, object_nunique))
+
+# Print number of unique entries by column, in ascending order
+sorted(d.items(), key=lambda x: x[1])
+The output above shows, for each column with categorical data, the number of unique values in the column
+
+number of unique values in the column = carcinality della categorical variable
+
+## Step 3: Investigating cardinality
+# Part A
+# Fill in the line below: How many categorical variables in the training data
+# have cardinality greater than 10?
+count = 0
+for i in d:
+    if d[i] > 10:
+        count += 1
+        
+high_cardinality_numcols = count
+print(high_cardinality_numcols)
+
+# Fill in the line below: How many columns are needed to one-hot encode the 
+# 'Neighborhood' variable in the training data?
+num_cols_neighborhood = d['Neighborhood']
+print(num_cols_neighborhood)
+
+# Part B
+# Fill in the line below: How many entries are added to the dataset by 
+# replacing the column with a one-hot encoding?
+OH_entries_added = 99*10000
+# Perchè conn 100 di cardinalità, dobbiamo aggiungere 99 colonne (una c'è già).
+# Avendo 10000 righe avremo un'aggiunta di un totale di 99*10000 entries al dataset
+
+# Fill in the line below: How many entries are added to the dataset by
+# replacing the column with an ordinal encoding?
+# nessuna, perché usi la colonna già esistente.
+label_entries_added = 0
+
+# you'll only create a one-hot encoding for columns with cardinality less than 10
+# Columns that will be one-hot encoded
+low_cardinality_cols = [col for col in object_cols if X_train[col].nunique() < 10]
+
+# Columns that will be dropped from the dataset
+high_cardinality_cols = list(set(object_cols)-set(low_cardinality_cols))
+
+print('Categorical columns that will be one-hot encoded:', low_cardinality_cols)
+print('\nCategorical columns that will be dropped from the dataset:', high_cardinality_cols)
+
+# Step 4: One-hot encoding
+# Apply one-hot encoder to each column with categorical data
+OH_encoder = OneHotEncoder(handle_unknown='ignore', sparse=False)
+OH_cols_train = pd.DataFrame(OH_encoder.fit_transform(X_train[low_cardinality_cols]))
+OH_cols_valid = pd.DataFrame(OH_encoder.transform(X_valid[low_cardinality_cols]))
+
+# One-hot encoding removed index; put it back
+OH_cols_train.index = X_train.index
+OH_cols_valid.index = X_valid.index
+
+# Remove categorical columns (will replace with one-hot encoding)
+num_X_train = X_train.drop(object_cols, axis=1)
+num_X_valid = X_valid.drop(object_cols, axis=1)
+
+# Add one-hot encoded columns to numerical features
+OH_X_train = pd.concat([num_X_train, OH_cols_train], axis=1)
+OH_X_valid = pd.concat([num_X_valid, OH_cols_valid], axis=1)
+
+# Ensure all columns have string type
+OH_X_train.columns = OH_X_train.columns.astype(str)
+OH_X_valid.columns = OH_X_valid.columns.astype(str)
+
+print("MAE from Approach 3 (One-Hot Encoding):") 
+print(score_dataset(OH_X_train, OH_X_valid, y_train, y_valid))
+MAE from Approach 3 (One-Hot Encoding):
+17525.345719178084
+
+## PIPELINES ##
+# Theory
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# Read the data
+data = pd.read_csv('../input/melbourne-housing-snapshot/melb_data.csv')
+
+# Separate target from predictors
+y = data.Price
+X = data.drop(['Price'], axis=1)
+
+# Divide data into training and validation subsets
+X_train_full, X_valid_full, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2,
+                                                                random_state=0)
+
+# "Cardinality" means the number of unique values in a column
+# Select categorical columns with relatively low cardinality (convenient but arbitrary)
+categorical_cols = [cname for cname in X_train_full.columns if X_train_full[cname].nunique() < 10 and 
+                        X_train_full[cname].dtype == "object"]
+
+# Select numerical columns
+numerical_cols = [cname for cname in X_train_full.columns if X_train_full[cname].dtype in ['int64', 'float64']]
+
+# Keep selected columns only
+my_cols = categorical_cols + numerical_cols
+X_train = X_train_full[my_cols].copy()
+X_valid = X_valid_full[my_cols].copy()
+
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
+
+# Step 1: Define Preprocessing Steps
+# Preprocessing for numerical data
+numerical_transformer = SimpleImputer(strategy='constant')
+
+# Preprocessing for categorical data
+categorical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='most_frequent')),
+    ('onehot', OneHotEncoder(handle_unknown='ignore'))
+])
+
+# Bundle preprocessing for numerical and categorical data
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numerical_transformer, numerical_cols),
+        ('cat', categorical_transformer, categorical_cols)
+    ])
+# Step 2: Define the Model
+from sklearn.ensemble import RandomForestRegressor
+model = RandomForestRegressor(n_estimators=100, random_state=0)
+
+# Step 3: Create and Evaluate the Pipeline
+from sklearn.metrics import mean_absolute_error
+
+# Bundle preprocessing and modeling code in a pipeline
+my_pipeline = Pipeline(steps=[('preprocessor', preprocessor),
+                              ('model', model)
+                             ])
+
+# Preprocessing of training data, fit model 
+my_pipeline.fit(X_train, y_train)
+
+# Preprocessing of validation data, get predictions
+preds = my_pipeline.predict(X_valid)
+
+# Evaluate the model
+score = mean_absolute_error(y_valid, preds)
+print('MAE:', score)
+
+# Practice
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# Read the data
+X_full = pd.read_csv('../input/train.csv', index_col='Id')
+X_test_full = pd.read_csv('../input/test.csv', index_col='Id')
+
+# Remove rows with missing target, separate target from predictors
+X_full.dropna(axis=0, subset=['SalePrice'], inplace=True)
+y = X_full.SalePrice
+X_full.drop(['SalePrice'], axis=1, inplace=True)
+
+# Break off validation set from training data
+X_train_full, X_valid_full, y_train, y_valid = train_test_split(X_full, y, 
+                                                                train_size=0.8, test_size=0.2,
+                                                                random_state=0)
+
+# "Cardinality" means the number of unique values in a column
+# Select categorical columns with relatively low cardinality (convenient but arbitrary)
+categorical_cols = [cname for cname in X_train_full.columns if
+                    X_train_full[cname].nunique() < 10 and 
+                    X_train_full[cname].dtype == "object"]
+
+# Select numerical columns
+numerical_cols = [cname for cname in X_train_full.columns if 
+                X_train_full[cname].dtype in ['int64', 'float64']]
+
+# Keep selected columns only
+my_cols = categorical_cols + numerical_cols
+X_train = X_train_full[my_cols].copy()
+X_valid = X_valid_full[my_cols].copy()
+X_test = X_test_full[my_cols].copy()
+
+code from the tutorial to preprocess the data and train a model
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+
+# Preprocessing for numerical data
+numerical_transformer = SimpleImputer(strategy='constant')
+
+# Preprocessing for categorical data
+categorical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='most_frequent')),
+    ('onehot', OneHotEncoder(handle_unknown='ignore'))
+])
+
+# Bundle preprocessing for numerical and categorical data
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numerical_transformer, numerical_cols),
+        ('cat', categorical_transformer, categorical_cols)
+    ])
+
+# Define model
+model = RandomForestRegressor(n_estimators=100, random_state=0)
+
+# Bundle preprocessing and modeling code in a pipeline
+clf = Pipeline(steps=[('preprocessor', preprocessor),
+                      ('model', model)
+                     ])
+
+# Preprocessing of training data, fit model 
+clf.fit(X_train, y_train)
+
+# Preprocessing of validation data, get predictions
+preds = clf.predict(X_valid)
+
+print('MAE:', mean_absolute_error(y_valid, preds))
+
+my turn:
+# Preprocessing for numerical data
+numerical_transformer = SimpleImputer(strategy = 'constant')
+
+# Preprocessing for categorical data
+categorical_transformer = Pipeline( steps = [
+    ('imputer', SimpleImputer(strategy = 'constant')),
+    ('onehot', OneHotEncoder(handle_unknown = 'ignore'))
+])
+
+# Bundle preprocessing for numerical and categorical data
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numerical_transformer, numerical_cols),
+        ('cat', categorical_transformer, categorical_cols)
+    ])
+
+# Define model
+model = RandomForestRegressor(n_estimators=100, random_state=0)
+
+# Part B
+# Bundle preprocessing and modeling code in a pipeline
+my_pipeline = Pipeline(steps=[('preprocessor', preprocessor),
+                              ('model', model)
+                             ])
+
+# Preprocessing of training data, fit model 
+my_pipeline.fit(X_train, y_train)
+
+# Preprocessing of validation data, get predictions
+preds = my_pipeline.predict(X_valid)
+
+# Evaluate the model
+score = mean_absolute_error(y_valid, preds)
+print('MAE:', score)
+
+
+# Step 2: Generate test predictions
+# Preprocessing of test data, fit model
+preds_test = my_pipeline.predict(X_test)
 
 
